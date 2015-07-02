f54dadebe0115afc8b9f9489005a589e425d8fa0
darkestkhan <darkestkhan@gmail.com>
2015-07-02 16:37:56 +0000

    Relicense to from GPLv3+ to ISC License.
    Signed-off-by: darkestkhan <darkestkhan@gmail.com>

--- src/vt100.adb
+++ src/vt100.adb
@@ -1,22 +1,20 @@
-pragma License (GPL);
 ------------------------------------------------------------------------------
 -- EMAIL: <darkestkhan@gmail.com>                                           --
--- License: GNU GPLv3 or any later as published by Free Software Foundation --
--- (see README file)                                                        --
---                    Copyright © 2011 - 2012 darkestkhan                   --
+-- License: ISC License (see COPYING file)                                  --
+--                                                                          --
+--                    Copyright © 2011 - 2015 darkestkhan                   --
 ------------------------------------------------------------------------------
---  This Program is Free Software: You can redistribute it and/or modify    --
---  it under the terms of The GNU General Public License as published by    --
---    the Free Software Foundation, either version 3 of the license, or     --
---                (at Your option) any later version.                       --
+-- Permission to use, copy, modify, and/or distribute this software for any --
+-- purpose with or without fee is hereby granted, provided that the above   --
+-- copyright notice and this permission notice appear in all copies.        --
 --                                                                          --
---      This Program is distributed in the hope that it will be useful,     --
---      but WITHOUT ANY WARRANTY; without even the implied warranty of      --
---      MERCHANTABILITY or FITNESS for A PARTICULAR PURPOSE. See the        --
---              GNU General Public License for more details.                --
---                                                                          --
---    You should have received a copy of the GNU General Public License     --
---   along with this program. If not, see <http://www.gnu.org/licenses/>.   --
+-- The software is provided "as is" and the author disclaims all warranties --
+-- with regard to this software including all implied warranties of         --
+-- merchantability and fitness. In no event shall the author be liable for  --
+-- any special, direct, indirect, or consequential damages or any damages   --
+-- whatsoever resulting from loss of use, data or profits, whether in an    --
+-- action of contract, negligence or other tortious action, arising out of  --
+-- or in connection with the use or performance of this software.           --
 ------------------------------------------------------------------------------
 -- TODO:
 --  - finish adding escape codes [cursor controls]
